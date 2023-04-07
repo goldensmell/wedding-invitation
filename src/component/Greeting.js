0251af6b5b5ca37dac8b13513524f6b7b3a794a2
golden <ohsdsl@gmail.com>
2023-04-08 00:05:18 +0900

    인트로 위치 조정, 인삿말 css 추가

--- src/component/Greeting.js
+++ src/component/Greeting.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Greeting.css'
-
+import flower from '../images/greeting-flower.png'
 export default function Greeting() {
   return (
     <div className='greeting'>
-      <p className='greeting-title'>결혼합니다</p>
+      {/* <div className='greeting-outline'></div> */}
+      <img className='greeting-flower' src={flower}></img>
+      <p className='greeting-title'>INVITATION</p>
       <div className='greeting-message'>
           <p>서로가 마주보며 다져온 사랑을 </p>
           <p>이제 함께 한 곳을 바라보며 걸어갈 수 있는</p>
@@ -14,8 +16,8 @@ export default function Greeting() {
           <p>앞날을 축복해 주시면 감사하겠습니다.</p>
       </div>
       <div className='greeting-family'>
-        <p>홍길동 홍길동의 차남 홍길동</p>
-        <p>홍길동 홍길동의 장녀 홍길동</p>
+        <p>홍길동 • 홍길동의 아들 &nbsp;&nbsp;홍길동</p>
+        <p>홍길동 • 홍길동의 &nbsp;&nbsp;&nbsp;&nbsp;딸 &nbsp;&nbsp;홍길동</p>
       </div> 
     </div>
   )
